e994ddb065ceebf54939147e00502381a1d5060d
Bo Zhang <bo@gradle.com>
2019-05-23 08:15:08 +0800

    Add README

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,44 @@
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.io.IOUtils;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.stream.Stream;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.IOUtils;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         a();
     }
 
-    private static void a() throws IOException {
+    public static void a() throws IOException {
         b();
     }
 
-    private static void b() throws IOException {
+    public static void b() throws IOException {
         c();
     }
 
-    private static void c() throws IOException {
+    public static void c() throws IOException {
         d();
     }
 
-    private static String d() throws IOException {
-        Stream<String> entries = Stream.of(System.getProperty("java.class.path").split(File.pathSeparator));
-        File targetClassDir = entries.filter(entry -> entry.endsWith("target/classes")).findFirst().map(File::new).orElseThrow(IllegalStateException::new);
+    public static String d() throws IOException {
+        // Please set a breakpoint here to catch the value of result
+        // 请在这里设置一个断点，捕捉result的值
+        String result = DigestUtils.md5Hex(readFileContent());
+        return result;
+    }
+
+    public static String readFileContent() throws IOException {
+        Stream<String> entries =
+                Stream.of(System.getProperty("java.class.path").split(File.pathSeparator));
+        File targetClassDir =
+                entries.filter(entry -> entry.endsWith("target/classes"))
+                        .findFirst()
+                        .map(File::new)
+                        .orElseThrow(IllegalStateException::new);
         File mainDotJava = new File(targetClassDir, "../../src/main/java/Main.java");
 
-        String sanitizedFileContent = IOUtils.toString(new FileReader(mainDotJava)).replaceAll("\\s", "");
-        String result = DigestUtils.md5Hex(sanitizedFileContent);
-        return result;
+        return IOUtils.toString(new FileReader(mainDotJava)).replaceAll("\\s", "");
     }
 }
